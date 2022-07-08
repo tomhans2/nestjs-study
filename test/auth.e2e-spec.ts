3133db501afcd2bf2c5454ef64ec8cf26847c67f
tomhans2 <hanshuangshi@126.com>
2022-07-08 13:20:35 +0800

    finish the user module

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -25,4 +25,20 @@ describe('Authentication System (e2e)', () => {
     expect(id).toBeDefined();
     expect(email_1).toEqual(email_1);
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = '1233@1233.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: email, password: '123' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+    expect(body.email).toEqual(email);
+  });
 });
